d47c572a90369e0bb038d14acf0524da61fc2927
nazanintp <nazaninghafouriann@gmail.com>
2026-01-02 10:36:13 -0800

    Update copyright year to 2025 and add production deployment configuration

--- sustindex/settings.py
+++ sustindex/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-mk-g2ptinu4*8ojldf$f*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['*']  # Render will handle this
+ALLOWED_HOSTS = ['*']  # Allow access from all devices
 
 
 # Application definition
